3640ec6e25d2ad460345d5b175557b53231122a2
migue <miguelexd@hotmail.com>
2021-03-31 17:49:06 +0200

    Commit inicial

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Escritorio')
+
+@section('content_header')
+    <h1>Escritorio</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido al escritorio de la aplicación.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
