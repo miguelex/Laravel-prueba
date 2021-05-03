f6505a8abc55a49c1110f3911197d19e00c7e6bb
migue <miguelexd@hotmail.com>
2021-05-03 11:29:15 +0200

    fix caras

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,3 +21,5 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/logout', [AuthController::class, 'logout']);
 
 Route::apiResource('empleados', App\Http\Controllers\Api\EmpleadoController::class);
+
+Route::apiResource('caras', App\Http\Controllers\Api\CaraController::class);
